3ffe3b194d65cad1c0664e04e7f0d3b8464ab67e
j.gosztyla <goszjacek@gmail.com>
2020-10-21 18:42:39 +0200

    add addItem feature

--- src/main/scala/EShop/lab2/CartActor.scala
+++ src/main/scala/EShop/lab2/CartActor.scala
@@ -1,6 +1,7 @@
 package EShop.lab2
 
-import akka.actor.{Actor, ActorRef, Cancellable, Props}
+import akka.Done
+import akka.actor.{Actor, ActorRef, ActorSystem, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
 
 import scala.concurrent.duration._
@@ -25,18 +26,40 @@ object CartActor {
 class CartActor extends Actor {
 
   import CartActor._
+  var cart = Cart.empty
 
   private val log       = Logging(context.system, this)
   val cartTimerDuration = 5 seconds
 
   private def scheduleTimer: Cancellable = ???
 
-  def receive: Receive = ???
+  def receive: Receive = LoggingReceive{
+    case CartActor.AddItem(item) => {
+      cart.addItem(item)
+      // todo
+//      context become nonEmpty(cart, scheduleTimer)
+    }
+  }
 
   def empty: Receive = ???
 
-  def nonEmpty(cart: Cart, timer: Cancellable): Receive = ???
+  def nonEmpty(cart: Cart, timer: Cancellable): Receive = LoggingReceive {
+    case CartActor.AddItem(item) => {
+      cart.addItem(item)
+    }
+  }
 
   def inCheckout(cart: Cart): Receive = ???
+
+}
+
+
+object CartApp extends App{
+  val system = ActorSystem("CartSystem")
+  val mainActor = system.actorOf(Props[CartActor])
+
+  mainActor ! CartActor.AddItem(5)
+
+
 
 }
